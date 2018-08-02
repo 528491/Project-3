3135379e8f25ac98238b7440fef95537b8e45266
unknown <mattcmej@gmail.com>
2018-08-01 18:37:59 -0700

    Users route is now being hit by router.

--- server.js
+++ server.js
@@ -13,9 +13,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.post("/api/users", function(req, res) {
-  console.log("hit route.");
+// app.post("/api/users", function(req, res) {
+//   console.log("hit route.");
+// });
+
+// Enable CORS from client-side
+app.use(function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
 });
+
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/testdb");
