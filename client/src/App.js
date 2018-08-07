1baa367a3e3e49b5cd9fad405cd672763d2e36ea
Aaron Farber <aaronfarber9@gmail.com>
2018-08-07 15:58:22 -0700

    authed up

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,34 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom'
-import {
-  Fade
-} from 'react-router';
+// import {
+//   Fade
+// } from 'react-router';
 // import logo from './logo.svg';
 // import API from "./utils/API.js";
 import './App.css';
 // import "./pages/HelloWorld";
 import Home from "./pages/Home/Home";
 import SignUp from "./pages/SignUp/SignUp";
+import Test from "./pages/SignUp/Test";
+import axios from 'axios';
+
 
 class App extends Component {
 
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  //
+  // }
+
+
 
   state = {
-    tests: []
+    tests: [],
+    authenticated: false
   }
 
   // componentDidMount(){
@@ -30,13 +38,76 @@ class App extends Component {
   //   .catch(err=>console.log(err));
   // }
 
+  authenticated() {
+    return this.state.authenticated
+  }
+
+  authenticate = () => {
+    console.log(this.state);
+    this.setState({
+      authenticated: true
+    })
+  }
+
+  deAuthenticate() {
+    this.setState({
+      authenticated: false
+    })
+  }
+
+  redirect = () => {
+    return (<Redirect to='/signup' />);
+  }
+
+  login = () => {
+    console.log('login');
+    return (<Redirect to='/' />);
+  }
+
+  logout() {
+      axios.get('/apis/users/logout')
+        .then(function (data) {
+          this.deAuthenticate();
+          window.location.reload();
+        }.bind(this)).catch(function (err) {
+          console.log(err);
+        });
+  }
+
+
 
   render() {
+    const authenticated = this.state.authenticated;
+    console.log(this.state);
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/signup" component={SignUp}/>
+          <Route exact path="/" render={props =>
+            !this.state.authenticated ? this.redirect() :
+            <Home
+              {...props}
+              authenticate={this.authenticate}
+              deAuthenticate={this.deAuthenticate}
+              authenticated={this.state.authenticated}
+            />}
+          />
+        <Route exact path="/signup" render={props =>
+            <SignUp
+              {...props}
+              authenticate={this.authenticate}
+              deAuthenticate={this.deAuthenticate}
+              authenticated={this.state.authenticated}
+            />}
+          />
+        <Route exact path="/test" render={props =>
+            !this.state.authenticated ? this.redirect() :
+              <Test
+                {...props}
+                authenticate={this.authenticate}
+                deAuthenticate={this.deAuthenticate}
+                authenticated={this.state.authenticated}
+              />}
+          />
         </Switch>
       </Router>
     );
