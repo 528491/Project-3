1c2ee135ed654d178f4bbcbe6356236334e63c0c
528491 <jacobminasian@gmail.com>
2018-08-07 16:40:31 -0700

    Commiting just to be safe

--- routes/api/tests.js
+++ routes/api/tests.js
@@ -4,7 +4,5 @@ const testController = require("../../controllers/testController");
 // Matches with "/api/tests"
 router.route("/")
   .get(testController.findAll);
-  
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app            = express();
 const session        = require("express-session");
 const passport 			 = require("./config/passport");
 const authCheck      = require("./config/middleware/attachAuthenticationStatus");
+const path           = require("path");
 
 
 const PORT = process.env.PORT || 3001;
@@ -14,9 +15,13 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
+/*
+This block is commented out as I want to see if a "catch-all" type routing hierarchy will serve us better.
+This is the method employed in react hw #2. The only change is that we place this app.use AFTER app.use(routes)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+*/
 // Add routes, both API and view
 // app.post("/api/users", function(req, res) {
 //   console.log("hit route.");
@@ -42,6 +47,10 @@ require("./config/databaseImplementation");
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/testdb");
 app.use(routes);
+app.use(express.static("client/build"));
+app.get("*", function(req, res){
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 
 // Start the API server
