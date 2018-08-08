d7095a99d23a2747f301b7ccd6b369cb8bf3692c
unknown <mattcmej@gmail.com>
2018-08-07 18:21:25 -0700

    In process of adding calendar page.

---
+++ client/src/pages/CalendarDisplay/CalendarDisplay.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Calendar from "./components/Calendar";
+
+import "./CalendarDisplay.css";
+
+class CalendarDisplay extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              react<b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default CalendarDisplay;
