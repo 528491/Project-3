34b4e01c8481b19e5233ac919be1af5782514891
unknown <mattcmej@gmail.com>
2018-08-09 21:41:53 -0700

    Clean up code in EventForm.js page. Also add helpful comments.

--- client/src/pages/EventForm/EventForm.js
+++ client/src/pages/EventForm/EventForm.js
@@ -12,139 +12,87 @@ class EventForm extends Component {
     constructor(props) {
         super(props);
         console.log(props);
+
+        // Set all state key-value pairs equal to the page's corresponding parameters.
         this.state = {
-            // username: "",
-            // year: this.props.year,
             year: this.props.match.params.year,
-            // month: this.props.month,
             month: this.props.match.params.month,
-            // day: this.props.day,
             day: this.props.match.params.day,
+
+            // User will determine this value via the form.
             guardianName: "",
+
+            // We will be pushing the day's events into this array.
             events: [],
+
+            // User will determine this value via the form.
             userEvent: ""
         };
     }
 
     componentDidMount() {
-        // this.loadEvents(this.state.year, this.state.month, this.state.day);
         this.loadEvents();
     }
 
+    // Copy-pasted from Sign Up form.
     handleInputChange = event => {
         const { name, value } = event.target;
-        // const guardianName = event.target.guardianName;
-        // const userEvent = event.target.userEvent;
         this.setState({
           [name]: value
-            // guardianName
         });
     };
 
+    // When the form is submitted...
     handleFormSubmit = event => {
         event.preventDefault();
         console.log("Item submitted!");
+
+        // Post an object to the api/events route.
         axios.post("http://localhost:3001/api/events",
+
+        // The object has the following key-value pairs.
         {
-        //   email: this.state.email,
-        //   password: this.state.password
             year: this.state.year,
             month: this.state.month,
             day: this.state.day,
             guardianName: this.state.guardianName,
             userEvent: this.state.userEvent
         })
-        // .then((data) => {
-  
-  
-        //   if(data.data.success) {
-        //       this.props.authenticate();
-              
-        //   }
-  
-  
-        // })
-        // .catch(error => {
-        //   console.log(error.response);
-        // });
+
     }
 
-    // loadEvents = (year, month, day) => {
-    //     API.getEvents(year, month, day)
-    //         .then(res =>
-    //             this.setState({events: res.data, guardianName: "", userEvent: ""})
-    //         )
-    //         .catch(err => console.log(err));
-    // } 
-
+    // Get request to load all the day's events
     loadEvents = () => {
+
+        // Perform a GET request to the URL.
+        // The URL will have queries specified by the "params" object.
         axios.get("http://localhost:3001/api/events", {
+
+            // Queries. These will filter out the items in the database.
             params: {
                 year: this.state.year,
                 month: this.state.month,
                 day: this.state.day
             }
         })
-        // .then(function(response) {
-        //     console.log(response);
-        // })
 
+        // Store the results into the "events" array,
+        // and clear the guardianName and userEvent fields.
         .then(res => 
             this.setState({events: res.data, guardianName: "", userEvent: ""}))
-        // .catch(function(error) {
-        //     console.log(error);
-        // });
+
 
         .catch(err => console.log(err));
 
     }
 
-    // addEvent = event => {
-    //     event.preventDefault();
-    //     axios.post("http://localhost:3001/api/events")
-    // }
 
-    // componentDidMount() {
-        // API.getDayEvents(this.props.match.params.id)
-        // .then(res => this.setState({ book: res.data }))
-        // .catch(err => console.log(err));
-    // }
-
-    // render() {
-    //     return(
-    //         <div>
-    //             <p>This is the event form.</p>
-    //         </div>
-    //     )
-    // }
 
     render() {
-        // if(this.props.authenticated) {
-        //   return (<Redirect to='/' />);
-        // }
-  
         return (
 
             <Container>
-
                 <Form>
-                    {/* <FormGroup>
-                        <Label for="exampleYear">Year</Label>
-                        <Input type="year" name="year" id="exampleYear" placeholder="with a placeholder"
-                        onChange = {this.handleInputChange}/>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label for="exampleMonth">Month</Label>
-                        <Input type="month" name="month" id="exampleMonth" placeholder="with a placeholder"
-                        onChange = {this.handleInputChange}/>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label for="exampleDay">Day</Label>
-                        <Input type="day" name="day" id="exampleDay" placeholder="with a placeholder"
-                        onChange = {this.handleInputChange}/>
-                    </FormGroup> */}
 
                     <FormGroup>
                         <Label for="exampleGuardianName">Guardian's Name</Label>
@@ -159,29 +107,34 @@ class EventForm extends Component {
                     </FormGroup>
 
                     <Button onClick={this.handleFormSubmit}>Create Event</Button>
-                    {/* <Button onClick = {this.handleLogin}>Log In</Button> */}
                 </Form>
 
                 <Space/>
                 <Space/>
 
+                {/* If there are items in the "events" array */}
                 {this.state.events.length ? (
-              <List>
-                {this.state.events.map(userEvent => (
-                  <ListItem key={userEvent._id}>
-                    {/* <Link to={"/books/" + book._id}> */}
-                      <strong>
-                        {userEvent.guardianName}
-                      </strong>
-                      <p>{userEvent.userEvent}</p>
-                    {/* </Link> */}
-                    {/* <DeleteBtn onClick={() => this.deleteBook(book._id)} /> */}
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
+                    <List>
+
+                        {/* Perform the "map" method on the "events" array. */}
+                        {/* Each event in the array with display to the page.*/}
+                        {this.state.events.map(userEvent => (
+                        <ListItem key={userEvent._id}>
+                            <strong>
+                                {userEvent.guardianName}
+                            </strong>
+                            <p>{userEvent.userEvent}</p>
+
+                        </ListItem>
+                        ))}
+                    </List>
+                ) 
+
+                // If there are no items in the "events" array, then...
+                : 
+                (
+                    <h3>No Results to Display</h3>
+                )}
   
             </Container>
         )
