9f0cc431c6b1b6a21e325a6b3edfd66a821f64ed
528491 <jacobminasian@gmail.com>
2018-08-15 00:52:49 -0700

    Discovered that we need to include app.close() inside every test case.

--- test/apiTests.js
+++ test/apiTests.js
@@ -1,6 +1,17 @@
 const test = require("tape");
 const request = require("supertest");
 const app = require("../server");
+const assert = require('assert');
+
+//Mocha and Chai tests
+// describe('Basic Mocha String Test', function () {
+//     it('should return number of charachters in a string', function () {
+//            assert.equal("Hello".length, 5);
+//        });
+//     it('should return first charachter of the string', function () {
+//            assert.equal("Hello".charAt(0), 'H');
+//        });
+//    });
 
 test("Test that tape and supertest are installed and functioning properly", function(t){
     t.end();
@@ -13,6 +24,7 @@ test("Verify we can visit the base route without any errors.", function(t){
         .expect(200)
         .end(function(err, res){
             t.error(err, "No Error");
+            app.close();
             t.end();
         })
 });
@@ -21,6 +33,8 @@ test("Verify that the base route returns type html and not .json", function(t){
     request(app)
         .get("/")
         .end(function(err, res){
+            t.error(err, "No Error");
+            app.close();
             t.end();
         });
 });
