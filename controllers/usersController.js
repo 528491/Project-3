fb9bae97e3344e28b94bcc393e88fb90860766ab
unknown <mattcmej@gmail.com>
2018-08-16 07:33:24 -0700

    Change signUp function's findOne method to look for email instead of username. Also add comments to code.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,22 +3,31 @@ const User       = require('../models/user');
 const Session    = require('../models/session');
 
 module.exports = {
+
+  // Function for registering a user on the website.
   signUp: function(req, res){
-    // console.log("req.body");
-    // res.send("<h1>Succesffully hit the usersController</h1>");
 
-    const newUser = new User();
+    // Attempts to find one username that matches the req.body.username.
+    User.findOne({ 'email' :  req.body.email }, function(err, user) {
 
-    newUser.email = req.body.email.toLowerCase().trim();
-    // newUser.password = req.body.password;
-    newUser.password = newUser.generateHash(req.body.password);
-
-    User.findOne({'email' : newUser.email}, function(err, user) {
+      // If the email the user specified is already in the database
       if (user) {
-        res.send({duplicateUser: true})
+        res.send ({duplicateUser: true})
       }
 
+      // If the user's email is NOT in the database
       else {
+
+        // console.log("req.body");
+        // res.send("<h1>Succesffully hit the usersController</h1>");
+
+        // Create a new User instance
+        const newUser = new User();
+        newUser.email = req.body.email.toLowerCase().trim();
+        // newUser.password = req.body.password;
+        newUser.password = newUser.generateHash(req.body.password);
+
+        // Saves the user to the database.
         newUser.save()
         .then(function() {
           res.send({success:true});
@@ -26,17 +35,25 @@ module.exports = {
         .catch(function(err) {
           res.json(err);
         });
+
       }
     })
-
-    
   },
 
+  ////////////////////////////////////////////////////////////////////////////////
+
+
+  // Function for logging in user to the website.
   login: function(req, res){
     console.log("Currently inside usersController login function");
+
+    // email and password variable will be specified by lines of code (req.body)
     let { email, password } = req.body;
+
+    // makes email field lowercase
     email = email.toLowerCase();
 
+    // If there is no "email" variable (If this field is blank maybe)
     if(!email) {
       return res.send({
         success: false,
@@ -44,6 +61,7 @@ module.exports = {
       })
     }
 
+    // Same as above, but for password field
     if(!password) {
       return res.send({
         success: false,
@@ -51,12 +69,12 @@ module.exports = {
       });
     }
 
-
-
+    // Find an email with the user's email.
     User.find({
       email: email
     }, (err, users) => {
 
+      // If there is a general error
       if(err) {
         console.log(err);
         res.send({
@@ -67,17 +85,23 @@ module.exports = {
       }
 
       console.log(users);
+
+      // If there isn't exactly one of the user's email in the database
+      // Safeguard against duplicates in the database
       if(users.length != 1) {
         console.log("users: ", users);
         return res.send({
           success: false,
           message: 'Error: Invalid',
+          
+          // Error will have an array of the duplicate users
           error: ["User find error", ...users]
         })
       }
 
       const user = users[0];
 
+      // If the user doesn't have a valid password
       if(!user.validPassword(password)) {
         console.log("p dub fail")
         return res.send({
@@ -86,6 +110,7 @@ module.exports = {
         });
       }
 
+      // User's token
       const token = Session.generateToken(user);
 
       console.log("logged in");
