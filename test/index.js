cd0014e434a26bf65abb505ffb963972e0197186
528491 <jacobminasian@gmail.com>
2018-08-16 00:51:12 -0700

    Discovered that Mocha version 3 plays much more nicely than express - hence we switched versions. In addition, we also discovered that using Mocha it statements as opposed to tape test statements is much better for our tests.

---
+++ test/index.js
@@ -0,0 +1,62 @@
+const test = require("tape");
+const request = require("supertest");
+const app = require("../server");
+const assert = require('assert');
+
+//Mocha and Chai tests
+// describe('Basic Mocha String Test', function () {
+//     it('should return number of charachters in a string', function () {
+//            assert.equal("Hello".length, 5);
+//        });
+//     it('should return first charachter of the string', function () {
+//            assert.equal("Hello".charAt(0), 'H');
+//        });
+//    });
+
+it("Trivial test to make sure that we successfully imported Mocha", function(done){
+    done();
+});
+
+it("Verify we can visit the base route without any errors.", function(done){
+    request(app)
+        .get("/")
+        .end(function(err, res){
+            done();
+        });
+
+});
+
+// test("Verify that the base route returns type html and not .json", function(t){
+//     request(app)
+//         .get("/")
+//         .expect('Content-Type', /html/)
+//         .expect(200)
+//         .end(function(err, res){
+//             t.error(err, "No Error");
+//             // app.close();
+//             t.end();
+//         });
+// });
+
+// test("Verify that the route /api/tests returns a json response", function(t){
+//     request(app)
+//         .get("/api/test")
+//         .expect("Content-Type", /json/)
+//         .expect(200);
+//     t.ok();
+//     // app.close();
+// });
+
+// //Only one response from /api.tests
+// //Will be used for testing other api routes as well
+
+// test("Test that /api/tests route returns a json response with only a single object", function(t){
+//     request(app)
+//         .get("/api/test")
+//         .end(function(err, res){
+//             t.error(err, 'No error');
+//             t.same(3, 3, "Fake numbers test");
+//             t.end();
+//             // app.close();
+//         });
+// });
